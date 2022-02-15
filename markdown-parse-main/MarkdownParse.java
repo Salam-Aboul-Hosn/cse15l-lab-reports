3d3ab3c27d51e66ffdeb33498f096bd76059d5ed
Salam-Aboul-Hosn <97714894+Salam-Aboul-Hosn@users.noreply.github.com>
2022-02-15 15:12:10 -0800

    Add files via upload

--- markdown-parse-main/MarkdownParse.java
+++ markdown-parse-main/MarkdownParse.java
@@ -1,8 +1,10 @@
+
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
@@ -10,19 +12,47 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while (currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if (nextOpenBracket == -1)
+                break;
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if (nextCloseBracket == -1)
+                break;
+            String betweenBracket = markdown.substring(nextOpenBracket + 1,
+                    nextCloseBracket);
+            if (betweenBracket.equals("!Image")
+                    || betweenBracket.equals("!File")) {
+                break;
+            }
+            currentIndex = nextCloseBracket + 1;
+            if (nextCloseBracket < markdown.length() + 1
+                    && markdown.charAt(nextCloseBracket + 1) == '(') {
+                int nextCloseParen = markdown.indexOf(')',
+                        nextCloseBracket + 1);
+                if (nextCloseParen == -1)
+                    break;
+                if (nextCloseParen - nextCloseBracket + 2 > 0) {
+                    String possURL = markdown.substring(nextCloseBracket + 2,
+                            nextCloseParen);
+                    if (Pattern.matches(
+                            "[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]",
+                            possURL))
+                        toReturn.add(possURL);
+                }
+                currentIndex = nextCloseParen + 1;
+            }
         }
         return toReturn;
     }
+
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        if (args.length == 0 || args == null) {
+            System.out.println("Please provide a file to parse");
+            return;
+        }
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
--- markdown-parse-main/MarkdownParseTest.java
+++ markdown-parse-main/MarkdownParseTest.java
@@ -1,22 +1,63 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.*; //import the junit Assert method statically
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.ArrayList;
 
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(99999999, 1 + 1);
+import org.junit.*; //import the junit library
+
+public class MarkdownParseTest { // create a class called MarkdownParseTest
+    @Test // Annotation to tell java to test
+    public void addition() { // test method
+        assertEquals(2, 1 + 1); // the test
     }
 
     @Test
-    public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("./test-file.md"));
-        List<String> expect = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void testGetLinks() throws IOException {
+        String[] args = { "test-file.md" };
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(links.toString(),
+                "[https://something.com, some-page.html]");
     }
-    
+
+    @Test
+    public void testGetLinks2() throws IOException {
+        String[] args = { "new-test.md" };
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(links.toString(),
+                "[https://www.google.com, www.bing.com]");
+    }
+
+    @Test
+    public void testGetLinks4() throws IOException {
+        String[] args = { "test-file9.md" };
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        System.out.println(links);
+        assertEquals("[whatever]", links.toString());
+    }
+
+    // test all 8 test files
+    @Test
+    public void testGetLinks3() throws IOException {
+        String[] expected = { "[https://something.com, some-page.html]", "[]",
+                "[]", "[]", "[page.com]", "[]", "[]" };
+        for (int i = 2; i < 9; i++) {
+            String[] args = { "test-file" + i + ".md" };
+            Path fileName = Path.of(args[0]);
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            System.out.println(links.toString());
+            if (i - 2 < expected.length)
+                assertEquals(links.toString(), expected[i - 2]);
+            else
+                assertEquals(links.toString(), "");
+        }
+    }
 }
